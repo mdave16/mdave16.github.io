eb29cbd0d56e31a251ac73bb684485dd6702ef08
mdave16 <mdave16@users.noreply.github.com>
2021-06-01 23:15:13 +0000

    deploy: ca41bcfc68b04f9aea0d07b00a17964a8982679e

--- assets/js/tic-tac-toe.js
+++ assets/js/tic-tac-toe.js
@@ -1,36 +1,35 @@
-let player = "X";
 let score = { X: 0, O: 0 };
+const players = Object.keys(score);
+let player = players[0];
 const winningCombos = [
-	// horizontal
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
-	// vertical
 	[0, 3, 6],
 	[1, 4, 7],
 	[2, 5, 8],
-	//diagonal
 	[0, 4, 8],
 	[2, 4, 6],
 ];
 const gameOverlay = document.getElementById("game-overlay");
 const cells = Array.from(document.getElementsByClassName("cell"));
 const info = document.getElementById("info");
+const grid = document.getElementById("grid");
 const scoreDisplay = document.getElementById("score");
 
 const winner = () => {
-	const grid = cells.map(cell => cell.dataset.played);
-	const combo = winningCombos.filter(
+	const grid = cells.map(cell => cell.dataset.played)
+	return winningCombos.some(
 		combo =>
 			grid[combo[0]] === grid[combo[1]] &&
 			grid[combo[0]] === grid[combo[2]] &&
 			grid[combo[0]] !== ""
 	);
-	return combo.length > 0;
 };
-const over = () => cells.map(c => c.dataset.played).every(c => c !== "");
+const over = () => cells.every(c => c.dataset.played !== "");
 const reset = () => {
 	cells.map(cell => (cell.dataset.played = ""));
+	grid.dataset.turn = player;
 	gameOverlay.classList.toggle("hidden");
 };
 const play = cell => {
@@ -43,12 +42,15 @@ const play = cell => {
 			gameOverlay.classList.toggle("hidden");
 			info.innerText = `Congratulations! The winner is: ${player}.`;
 			score[player]++;
-			scoreDisplay.innerText = `Current score: X: ${score["X"]} – O: ${score["O"]}`;
+			scoreDisplay.innerText = `Current score: ${players[0]}: ${score[players[0]]} – ${players[1]}: ${score[players[1]]}`;
 		} else if (over()) {
 			gameOverlay.classList.toggle("hidden");
 			info.innerText = `Game over. Try again.`;
 		} else {
-			player = player === "X" ? "O" : "X";
+			player = player === players[0] ? players[1] : players[0];
+			grid.dataset.turn = player;
 		}
 	}
 };
+
+reset();
